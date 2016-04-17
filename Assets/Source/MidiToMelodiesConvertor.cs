c1f2898cfb0150c75e30886a2d96d6acde567354
Michiel Huygen <mhgamework@gmail.com>
2016-04-17 19:41:27 +0200

    Added midi library

---
+++ Assets/Source/MidiToMelodiesConvertor.cs
@@ -0,0 +1,22 @@
+﻿using Multimedia.Midi;
+using UnityEngine;
+
+namespace Assets.Source
+{
+    public class MidiToMelodiesConvertor : MonoBehaviour
+    {
+        public void Awake()
+        {
+
+        }
+
+        public void OnEnable()
+        {
+            var reader = new MidiFileReader(@"C:\_Speedy\LudumDare35\Assets\Audio\Soundtrack01\LD35\LD35.mid");
+
+
+            int a = 5;
+
+        }
+    }
+}
